be54fdfb9c4405762c0b4e23c1e7d6a753397083
Ren <algorenstudio@gmail.com>
2020-08-01 01:01:54 -0400

    Added some code to make heroku work.

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -4,13 +4,13 @@ const connection;
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "hardfliprails",
-    database: "burgers_db"
-  });
+  connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "hardfliprails",
+  database: "burgers_db"
+});
 }
 
 connection.connect(function(err) {
